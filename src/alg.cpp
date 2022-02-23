3d089de586fe03706e072e66f8f5ceb3841ed746
Victoria Terganova <94222399+Terganova21@users.noreply.github.com>
2022-02-23 23:15:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,12 +6,14 @@
 bool checkPrime(uint64_t value) {
   if (value > 1) {
     for (int i = 2; i * i <= value; i++) {
-      if (value % i == 0) {
+      if (value % i == 0)
         return false;
       }
-      else {
-        return false;
-      }
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -34,7 +36,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  int sum = 0;
+  uint64_t sum = 0;
   hbound--;
   for (hbound; hbound > 1; hbound--) {
     if (checkPrime(hbound))
